e8163804dbd7c85029d9567b1f641fb5908fb9cf
= <=>
2024-08-13 13:10:11 +0700

    te123 123213

--- src/main/java/com/example/test_deloy/Controller/UserController.java
+++ src/main/java/com/example/test_deloy/Controller/UserController.java
@@ -1,9 +1,6 @@
 package com.example.test_deloy.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -15,8 +12,8 @@ public class UserController {
     }
 
     @PostMapping
-    public String update() {
-        return "updated success !";
+    public String update(@RequestParam String str) {
+        return str;
     }
 
     @GetMapping("/file")
