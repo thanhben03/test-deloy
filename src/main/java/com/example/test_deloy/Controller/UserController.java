ae8e890472202e093b52778b84d51c0f83cb755a
= <=>
2024-08-13 12:42:00 +0700

    tessttt

---
+++ src/main/java/com/example/test_deloy/Controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.test_deloy.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @GetMapping
+    private String test() {
+        return "ahiihi";
+    }
+}
