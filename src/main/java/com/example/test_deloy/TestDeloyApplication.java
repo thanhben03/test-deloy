a6813dca6d9539e9b2f166f2ecdab93dda0e2e39
= <=>
2024-08-13 11:28:18 +0700

    initial

---
+++ src/main/java/com/example/test_deloy/TestDeloyApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_deloy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeloyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeloyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_deloy/TestDeloyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_deloy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDeloyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
